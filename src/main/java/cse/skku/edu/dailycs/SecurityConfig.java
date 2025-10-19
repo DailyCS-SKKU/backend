995b5e4c7b6e326d5e66981de298346bcf6ff0b0
신진건 <sjg729729@gmail.com>
2025-10-19 14:56:00 +0900

    [Feat] CORS 설정

--- src/main/java/cse/skku/edu/dailycs/SecurityConfig.java
+++ src/main/java/cse/skku/edu/dailycs/SecurityConfig.java
@@ -4,6 +4,7 @@ import cse.skku.edu.dailycs.util.auth.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,7 +24,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .cors(AbstractHttpConfigurer::disable)
+            .cors(Customizer.withDefaults())
             .csrf(AbstractHttpConfigurer::disable) // CSRF 보호 비활성화
             .authorizeHttpRequests(auth -> auth
                     .requestMatchers("/auth/**").permitAll()
